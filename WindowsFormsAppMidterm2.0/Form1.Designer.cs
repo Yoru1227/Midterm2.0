54ddfb46c5ad21b1b235c86f514981eede35fa42
Yoru <Nokersx@gmail.com>
2023-11-24 08:54:40 +0800

    加入專案檔案。

---
+++ WindowsFormsAppMidterm2.0/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsAppMidterm2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRice = new System.Windows.Forms.Button();
+            this.btnNoodles = new System.Windows.Forms.Button();
+            this.btnSoup = new System.Windows.Forms.Button();
+            this.btnOthers = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 461);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 69);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.btnRice);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 69);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(188, 51);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.btnNoodles);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 120);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(188, 51);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel5.Controls.Add(this.btnSoup);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 171);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(188, 51);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel6.Controls.Add(this.btnOthers);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 222);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(188, 51);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel7.Location = new System.Drawing.Point(0, 404);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(188, 57);
+            this.panel7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(-2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OO小吃部";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRice
+            // 
+            this.btnRice.BackColor = System.Drawing.Color.White;
+            this.btnRice.Location = new System.Drawing.Point(-2, -2);
+            this.btnRice.Name = "btnRice";
+            this.btnRice.Size = new System.Drawing.Size(188, 51);
+            this.btnRice.TabIndex = 1;
+            this.btnRice.Text = "飯類";
+            this.btnRice.UseVisualStyleBackColor = false;
+            // 
+            // btnNoodles
+            // 
+            this.btnNoodles.BackColor = System.Drawing.Color.White;
+            this.btnNoodles.Location = new System.Drawing.Point(-2, -2);
+            this.btnNoodles.Name = "btnNoodles";
+            this.btnNoodles.Size = new System.Drawing.Size(188, 51);
+            this.btnNoodles.TabIndex = 2;
+            this.btnNoodles.Text = "麵類";
+            this.btnNoodles.UseVisualStyleBackColor = false;
+            // 
+            // btnSoup
+            // 
+            this.btnSoup.BackColor = System.Drawing.Color.White;
+            this.btnSoup.Location = new System.Drawing.Point(-2, -2);
+            this.btnSoup.Name = "btnSoup";
+            this.btnSoup.Size = new System.Drawing.Size(188, 51);
+            this.btnSoup.TabIndex = 2;
+            this.btnSoup.Text = "湯品";
+            this.btnSoup.UseVisualStyleBackColor = false;
+            // 
+            // btnOthers
+            // 
+            this.btnOthers.BackColor = System.Drawing.Color.White;
+            this.btnOthers.Location = new System.Drawing.Point(-2, -2);
+            this.btnOthers.Name = "btnOthers";
+            this.btnOthers.Size = new System.Drawing.Size(188, 51);
+            this.btnOthers.TabIndex = 2;
+            this.btnOthers.Text = "小菜";
+            this.btnOthers.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRice;
+        private System.Windows.Forms.Button btnOthers;
+        private System.Windows.Forms.Button btnSoup;
+        private System.Windows.Forms.Button btnNoodles;
+    }
+}
+
