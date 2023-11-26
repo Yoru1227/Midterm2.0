b7494115cd456fff1d06d8886230ee9387d68b46
Yoru <Nokersx@gmail.com>
2023-11-26 15:20:00 +0800

    照片更新

--- WindowsFormsAppMidterm2.0/GlobalVar.cs
+++ WindowsFormsAppMidterm2.0/GlobalVar.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsAppMidterm2._0
     internal class GlobalVar
     {
         public static string strMyDBConnectionString = "";
-        public static string imageDirHome = "C:\\Users\\user\\Desktop\\全端工程師\\Midterm2.0\\pics";
+        public static string imageDirHome = "C:\\Users\\user\\Desktop\\全端工程師\\Midterm2.0\\Midterm2.0\\pics";
         public static string imageDirWork = "";
     }
 }
